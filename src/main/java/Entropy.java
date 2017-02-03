c3e29d67b132dced7070f2d5d13a1e59c46054c7
Saber <SaberMeyt@gmail.com>
2017-02-04 02:29:37 +0300

    Закончены основные параметры, заготовки для суммарного ранга, продолжу в субботу вечером

--- src/main/java/Entropy.java
+++ src/main/java/Entropy.java
@@ -87,16 +87,16 @@ public class Entropy {
     //<не> конец
 
     public void getWW(){
-        double p = (double) 1 / ramoses.size();
-        double q = (double) (ramoses.size() - 1) / ramoses.size();
+        double p = (double) 1 / (double)ramoses.size();
+        double q = (double) ((double)ramoses.size() - (double)1) / (double)ramoses.size();
         Set<String> allKeys = allReferencies.keySet();
         for(String word: allKeys){
             double probWX = 0;
             for(Ramos ramos:ramoses){
                 if(ramos.wordCount.containsKey(word)){
                     double prob1=((double)allReferencies.get(word)/(double)ramos.wordCount.get(word))*
-                            Math.pow(p,ramos.wordCount.get(word))*Math.pow(q,(allReferencies.get(word)-ramos.wordCount.get(word)));
-                    double probWD=Math.pow(2,(double)-1*((double)Math.log10(prob1)/(double)Math.log10(2)));
+                            Math.pow(p,(double)ramos.wordCount.get(word))*Math.pow(q,((double)allReferencies.get(word)-(double)ramos.wordCount.get(word)));
+                    double probWD=Math.pow((double)2,(double)-1*((double)Math.log(prob1)/(double)Math.log(2)));
                     probWX+=probWD;
 
                 }
@@ -108,21 +108,62 @@ public class Entropy {
             for(Ramos ramos:ramoses){
                 if(ramos.wordCount.containsKey(word)){
                     double prob1=((double)allReferencies.get(word)/(double)ramos.wordCount.get(word))*
-                            Math.pow(p,ramos.wordCount.get(word))*Math.pow(q,(allReferencies.get(word)-ramos.wordCount.get(word)));
-                    double probWD=Math.pow(2,(double)-1*((double)Math.log10(prob1)/(double)Math.log10(2)));
+                            Math.pow(p,(double)ramos.wordCount.get(word))*Math.pow(q,(double)(allReferencies.get(word)-ramos.wordCount.get(word)));
+                    double probWD=Math.pow((double)2,(double)-1*((double)Math.log(prob1)/(double)Math.log(2)));
                     double probNorm = probWD/probWX;
                     ramos.probnorms.put(word,probNorm);
-                    Wrisk2wd = ((allReferencies.get(word))*((double)-1*(Math.log10(probNorm)/Math.log10(2))))/(documentCounter.get(word)*(ramos.wordCount.get(word)+1));
-                    Wrisk1wd = ((double)-1*(Math.log10(probNorm)/Math.log10(2))/(ramos.wordCount.get(word)+1));
+                    Wrisk2wd = ((allReferencies.get(word))*((double)-1*(Math.log(probNorm)/(double)Math.log(2))))/(documentCounter.get(word)*(ramos.wordCount.get(word)+1));
+                    Wrisk1wd = ((double)-1*(Math.log10(probNorm)/Math.log10(2))/((double)ramos.wordCount.get(word)+(double)1));
                     W2w+=Wrisk2wd;
                     W1w+=Wrisk1wd;
+
                 }
             }
             w1s.put(word,W1w);
             w2s.put(word,W2w);
+            double Dfe1 = 0;
+            double Dfe2 = 0;
+            double pdoc, Wrisk2norm;
+            for(Ramos ramos:ramoses){
+                if(ramos.wordCount.containsKey(word)){
+                    pdoc=((double)ramos.wordCount.get(word)/(double)allReferencies.get(word));
+                    Dfe1+=pdoc*(Math.log10(pdoc/ramos.probnorms.get(word))/Math.log10(2));
+                    Wrisk2wd = ((allReferencies.get(word))*((double)-1*(Math.log(ramos.probnorms.get(word))/(double)Math.log(2))))/(documentCounter.get(word)*(ramos.wordCount.get(word)+1));
+                    Wrisk2norm= Wrisk2wd/w2s.get(word);
+                    Dfe2+=pdoc*(Math.log10(pdoc/Wrisk2norm)/Math.log10(2));
+                }
+            }
+            dfe1.put(word,Dfe1);
+            dfe2.put(word,Dfe2);
+
+        }
+
+    }
+
+    private void getRang(HashMap<String, Double> hashMap) {
+        List<Map.Entry<String, Double>> hList = new ArrayList<Map.Entry<String, Double>>(hashMap.entrySet());
+        int j = 0;
+        //Создание сортированного списка пар ключ-значение из исходного словаря для последовательной обработки
+        hList.sort(new Comparator<Map.Entry<String, Double>>() {
+            @Override
+            public int compare(Map.Entry<String, Double> o1, Map.Entry<String, Double> o2) {
+                return Double.compare(o1.getValue(), o2.getValue());
+            }
+        });
+        double current = 0;
+        int number = 1;
+        LinkedHashMap<String,Integer> linkedHashMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Double> pare : hList) {
+            if( pare.getValue()!=current){
+                number++;
+            }
+            linkedHashMap.put(pare.getKey(),number);
         }
     }
 
+    public HashMap<String,Integer> getSumRang(){
+        return null;
+    }
 
     public int mapsCalculation() {
         this.allReferencies.clear();
